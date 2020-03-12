45ee8b621d1cdde119a7fb1f793f09c42c5e8e66
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-12 14:10:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,8 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-
+    artist = self.all.detect { |person| person.name = name }
+    artist ? artist : self.new(name)
   end
 
   def initialize(name)
@@ -27,6 +28,12 @@ class Artist
   def songs
     Song.all.select { |song| song.artist == self }
   end
+
+  def print_songs
+    self.songs.each { |song| puts song.name }
+  end
+
+  def 
 
 
 end
