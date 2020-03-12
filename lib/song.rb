f7ab8d758321747a6e64b184c9580adbb99aca01
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-12 14:13:45 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+  attr_accessor :artist, :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    name = filename.split(' - ')
+    song = self.new(name[1])
+    song.artist = name[0]
+    song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def initialize(name)
+    self.name = name
+    self.save
+  end
+  
+end
